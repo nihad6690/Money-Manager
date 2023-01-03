88168ae2a8a19d2cfc85f593b24b1b8f205a5c05
nihad6690 <nihalanidhruvesh@gmail.com>
2023-01-03 16:15:48 -0500

    connected the front end to the back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,92 @@
-import React, { useState } from 'react';
-import './ExpenseTracker.css'; // Import the CSS file
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios'
 
 function ExpenseTracker() {
   const [expenses, setExpenses] = useState([]);
-  const [description, setDescription] = useState('');
+  const [text, setText] = useState('');
   const [amount, setAmount] = useState(0);
   const [balance, setBalance] = useState(0);
 
-  const addExpense = event => {
+  useEffect(() => {
+    async function fetchExpenses() {
+      try {
+        const response = await axios.get("/api/v1/transactions")
+        const data = response.data.data
+        console.log(data)
+        setExpenses(data);
+        setBalance(data.reduce((total, expense) => total + expense.amount, 0));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchExpenses();
+  }, []);
+
+  async function addExpense(event) {
     event.preventDefault();
-    setExpenses([...expenses, { description, amount }]);
-    setDescription('');
-    setAmount(0);
-    setBalance(balance + parseInt(amount));
-  };
+    try {
+      const config = {
+        headers: { 'Content-Type': 'application/json' },
+      
+      }
+      
+      const response = await axios.post("/api/v1/transactions/", {text, amount}, config)
+      const data = response.data.data
+      console.log(data)
+      setExpenses([...expenses, data]);
+      setBalance(balance + data.amount);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function deleteExpense(id) {
+    try {
+      
+      const response = await axios.delete(`/api/v1/transactions/${id}`);
+      const data = response.data.data
+      console.log(data)
+      setExpenses(expenses.filter(expense => expense._id !== id));
+      setBalance(balance - data.amount);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
 
   return (
     <div className="expense-tracker">
-      <h1 className="expense-tracker-title">Expense Tracker</h1>
-      <div
-        className={`balance ${balance >= 0 ? 'positive' : 'negative'}`}
-      >
-        Balance: {balance}
-      </div>
-      <form className="expense-form" onSubmit={addExpense}>
-        <label className="expense-form-label">
-          Description:
+      <h1>Expense Tracker</h1>
+      <p className="balance">Balance: {balance}</p>
+      <form onSubmit={addExpense}>
+        <div className="expense-form">
           <input
-            className="expense-form-input"
             type="text"
-            placeholder="Enter a description"
-            value={description}
-            onChange={event => setDescription(event.target.value)}
+            placeholder="Description"
+            value={text}
+            onChange={event => setText(event.target.value)}
           />
-        </label>
-        <br />
-        <label className="expense-form-label">
-          Amount:
           <input
-            className="expense-form-input"
             type="number"
+            placeholder="Amount"
             value={amount}
-            onChange={event => setAmount(event.target.value)}
+            onChange={event => setAmount(parseInt(event.target.value))}
           />
-        </label>
-        <br />
-        <button className="expense-form-button" type="submit">
-          Add Expense
-        </button>
+          <button type="submit">Add Expense</button>
+        </div>
       </form>
-      <h2 className="expenses-title">Expenses</h2>
-      <ul className="expenses-list">
-        {expenses.map((expense, index) => (
-          <li className={`expense ${expense.amount < 0 ? 'negative' : 'positive'}`} key={index}>
-            {expense.description}: {expense.amount}
+      <h2>Expenses</h2>
+      <ul>
+        {expenses.map(expense => (
+          <li key={expense._id} className={expense.amount > 0 ? 'positive' : 'negative'}>
+            <span>{expense.text}</span>
+            <span>{expense.amount}</span>
+            <button onClick={() => deleteExpense(expense._id)}>Delete</button>
           </li>
         ))}
       </ul>
     </div>
   );
 }
-
 export default ExpenseTracker;
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 
 app.use(express.json())
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 
