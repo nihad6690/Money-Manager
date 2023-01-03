72959f3642217202d6ea5cf0fd11c1e12c611250
nihad6690 <nihalanidhruvesh@gmail.com>
2023-01-03 03:42:28 -0500

    made changes to the UI

--- src/App.js
+++ src/App.js
@@ -1,27 +1,63 @@
+import React, { useState } from 'react';
+import './ExpenseTracker.css'; // Import the CSS file
 
-import React from 'react';
-import { Header } from './components/Header';
-import { Balance } from './components/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses';
-import { TransactionList } from './components/TransactionList';
-import { AddTransaction } from './components/AddTransaction';
+function ExpenseTracker() {
+  const [expenses, setExpenses] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [balance, setBalance] = useState(0);
 
-import { GlobalProvider } from './context/GlobalState';
+  const addExpense = event => {
+    event.preventDefault();
+    setExpenses([...expenses, { description, amount }]);
+    setDescription('');
+    setAmount(0);
+    setBalance(balance + parseInt(amount));
+  };
 
-import './App.css';
-
-function App() {
   return (
-    <GlobalProvider>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
+    <div className="expense-tracker">
+      <h1 className="expense-tracker-title">Expense Tracker</h1>
+      <div
+        className={`balance ${balance >= 0 ? 'positive' : 'negative'}`}
+      >
+        Balance: {balance}
       </div>
-    </GlobalProvider>
+      <form className="expense-form" onSubmit={addExpense}>
+        <label className="expense-form-label">
+          Description:
+          <input
+            className="expense-form-input"
+            type="text"
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+          />
+        </label>
+        <br />
+        <label className="expense-form-label">
+          Amount:
+          <input
+            className="expense-form-input"
+            type="number"
+            value={amount}
+            onChange={event => setAmount(event.target.value)}
+          />
+        </label>
+        <br />
+        <button className="expense-form-button" type="submit">
+          Add Expense
+        </button>
+      </form>
+      <h2 className="expenses-title">Expenses</h2>
+      <ul className="expenses-list">
+        {expenses.map((expense, index) => (
+          <li className="expense" key={index}>
+            {expense.description}: {expense.amount}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default App;
+export default ExpenseTracker;
